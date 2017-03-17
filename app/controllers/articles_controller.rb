922dcec50ac0666c45a8f8c437ea11a94c2f1af9
Alpha-Log <alphalogmail@gmail.com>
2017-03-17 13:38:37 +0000

    Added Destroy and Partials with appropriate links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,12 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    def destroy
+       @article = Article.find(params[:id])
+       @article.destroy
+       flash[:notice] = "Arcitle was successfully deleted"
+       redirect_to articles_path
+    end
     
     private
         def article_params
