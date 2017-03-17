f4a55ece5ec6636cc85793dbcabf98c4829cd8c6
Alpha-Log <alphalogmail@gmail.com>
2017-03-17 13:51:20 +0000

    Extract redundant code in ArticleController - created private method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
     
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
+    
     def index
         @articles = Article.all
         
@@ -10,7 +13,7 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+        
     end
     def create
        @article = Article.new(article_params)
@@ -22,7 +25,6 @@ class ArticlesController < ApplicationController
        end
     end
     def update
-        @article = Article.find(params[:id])
        if @article.update(article_params)
            flash[:notice] = "Arcitle was successfully updated"
             redirect_to article_path(@article)
@@ -31,16 +33,18 @@ class ArticlesController < ApplicationController
        end
     end
     def show
-        @article = Article.find(params[:id])
+        
     end
     def destroy
-       @article = Article.find(params[:id])
        @article.destroy
        flash[:notice] = "Arcitle was successfully deleted"
        redirect_to articles_path
     end
     
     private
+    def set_article
+        @article = Article.find(params[:id])
+    end
         def article_params
             params.require(:article).permit(:title, :description)
         end
